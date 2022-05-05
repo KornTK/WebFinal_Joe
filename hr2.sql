e5d6a66455d9a6a91f6aa86a016bb3a850895dcf
KornTK <pokde111@gmail.com>
2022-05-05 20:29:08 +0700

    Update hr2.sql

--- hr2.sql
+++ hr2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 05, 2022 at 08:20 AM
+-- Generation Time: May 05, 2022 at 03:28 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
@@ -65,8 +65,8 @@ CREATE TABLE `guest` (
 INSERT INTO `guest` (`guest_id`, `stu_id`, `password`, `g_prefix`, `g_name`, `g_sname`, `g_overdue`, `g_room`, `img_slip`) VALUES
 (7, '6330611003', '1221', 'นาย', 'ไชยวัฒน์', 'แก่นเพ็ชร์', 100, '2.425', ''),
 (9, '6330611005', '123', 'นาย', 'ศิริชัย', 'ปานแก้ว', 350, '2.402', ''),
-(10, '6330611009', '1234', 'นาย', 'ศิริชัย', 'ปานแก้2', 350, '2.402', ''),
-(15, '6330611007', '1234', 'นาย', 'ธนกร', 'ประสมกิจ', 98, '999', '15.jpg');
+(15, '6330611007', '1234', 'นาย', 'ธนกร', 'ประสมกิจ', 98, '999', ''),
+(20, '6440344333', '1234', 'นาย', 'ทดสอบ', 'ทดสอบกุล', 44, '101', '');
 
 --
 -- Indexes for dumped tables
@@ -98,7 +98,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `guest`
 --
 ALTER TABLE `guest`
-  MODIFY `guest_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `guest_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
